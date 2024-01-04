1ff7ddf97ab741e8d59a49a135457c6619d370ba
chyonee <chiwon99881@gmail.com>
2024-01-05 08:45:21 +0900

    User microservice init

---
+++ src/main/java/springmsa/springmsa_api_gateway/SpringmsaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package springmsa.springmsa_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmsaApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmsaApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springmsa/springmsa_api_gateway/SpringmsaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package springmsa.springmsa_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmsaApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
